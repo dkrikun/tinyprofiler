c917a52547e2404fe089b409da0e3c9e43a6567b
Constantine Tarasenkov <constantine@protonmail.com>
2017-08-10 13:54:01 +0300

    prof* to macros for sizeof(string) to work

--- tinyprofiler.h
+++ tinyprofiler.h
@@ -1,6 +1,15 @@
 #pragma once
 
-#ifdef USE_TINYPROFILER
+#ifndef USE_TINYPROFILER
+
+static inline void profAlloc(size_t sample_count_per_thread) {}
+static inline void profPrintAndFree() {}
+static inline void profB(char * name) {}
+static inline void profE(char * name) {}
+static inline void profBmt(int tid, char * name) {}
+static inline void profEmt(int tid, char * name) {}
+
+#else // USE_TINYPROFILER
 
 #ifndef TINYPROFILER_MAX_NUM_OF_THREADS
 #define TINYPROFILER_MAX_NUM_OF_THREADS 4
@@ -12,6 +21,22 @@
 
 #ifndef TINYPROFILER_MAX_JSON_LINE_LENGTH
 #define TINYPROFILER_MAX_JSON_LINE_LENGTH 10000
+#endif
+
+#ifndef TINYPROFILER_OUTPUT_STRING
+#ifdef _WIN32
+#define TINYPROFILER_OUTPUT_STRING(x) OutputDebugStringA(x)
+#else
+#define TINYPROFILER_OUTPUT_STRING(x) fprintf(stderr, x)
+#endif
+#endif
+
+#ifndef TINYPROFILER_TIME_TO_TS
+#ifdef _WIN32
+#define TINYPROFILER_TIME_TO_TS(x) (unsigned long)(((double)(x) / (double)_tinyprofiler_perf_freq) * 1000000.0)
+#else
+#define TINYPROFILER_TIME_TO_TS(x) ((x) - _tinyprofiler_prof_time_start * 1000000UL)
+#endif
 #endif
 
 #ifdef _WIN32
@@ -54,7 +79,7 @@ static inline unsigned long long _prof_time() {
 static inline unsigned long _prof_time() {
   struct timeval tv;
   gettimeofday(&tv, NULL);
-  return 1000000UL * tv.tv_sec + tv.tv_usec;
+  return tv.tv_sec * 1000000UL + tv.tv_usec;
 }
 #endif
 
@@ -84,57 +109,42 @@ static inline void profAlloc(size_t sample_count_per_thread) {
   }
 }
 
-#ifndef _TINYPROFILER_DIFF
-#ifdef _WIN32
-#define _TINYPROFILER_DIFF(x) (unsigned long)(((double)(x) / (double)_tinyprofiler_perf_freq) * 1000000.0)
-#else
-#define _TINYPROFILER_DIFF(x) ((x) - (1000000UL * _tinyprofiler_prof_time_start))
-#endif
-#endif
-
-#ifndef _TINYPROFILER_PRINT
-#ifdef _WIN32
-#define _TINYPROFILER_PRINT(x) OutputDebugStringA(x)
-#else
-#define _TINYPROFILER_PRINT(x) fprintf(stderr, x)
-#endif
-#endif
-
 static inline void profPrintAndFree() {
   unsigned long self_t_begin = (unsigned long)_prof_time();
-  size_t line_byte_size = TINYPROFILER_MAX_JSON_LINE_LENGTH * sizeof(char);
+  size_t line_bytes = TINYPROFILER_MAX_JSON_LINE_LENGTH * sizeof(char);
   char * line = (char *)calloc(TINYPROFILER_MAX_JSON_LINE_LENGTH, sizeof(char));
-  snprintf(line, line_byte_size, "{\"traceEvents\":[{}\n"); _TINYPROFILER_PRINT(line);
+  snprintf(line, line_bytes, "{\"traceEvents\":[{}\n"); TINYPROFILER_OUTPUT_STRING(line);
   for (int t = 0; t < TINYPROFILER_MAX_NUM_OF_THREADS; t += 1) {
     for (size_t i = 0; i < _prof_data[t].sample_count; i += 1) {
       if (_prof_data[t].s[i].ph) {
-        snprintf(line, line_byte_size, ",{\"ph\":\"%c\",\"ts\":%lu,\"pid\":%d,\"tid\":%d,\"name\":\"%s\"}\n",
-                _prof_data[t].s[i].ph,  _TINYPROFILER_DIFF(_prof_data[t].s[i].ts),
-                _prof_data[t].s[i].pid, _prof_data[t].s[i].tid, _prof_data[t].s[i].name);
-        _TINYPROFILER_PRINT(line);
+        snprintf(line, line_bytes, ",{\"ph\":\"%c\",\"ts\":%lu,\"pid\":%d,\"tid\":%d,\"name\":\"%s\"}\n",
+                 _prof_data[t].s[i].ph,  TINYPROFILER_TIME_TO_TS(_prof_data[t].s[i].ts),
+                 _prof_data[t].s[i].pid, _prof_data[t].s[i].tid, _prof_data[t].s[i].name);
+        TINYPROFILER_OUTPUT_STRING(line);
       } else break;
     }
   }
-  for (int t = 0; t < TINYPROFILER_MAX_NUM_OF_THREADS; t += 1)
-    free(_prof_data[t].s);
-  snprintf(line, line_byte_size, ",{\"ph\":\"%c\",\"ts\":%lu,\"pid\":%d,\"tid\":%d,\"name\":\"%s\"}\n", 'B', _TINYPROFILER_DIFF((unsigned long)self_t_begin), 0, 0, __func__); _TINYPROFILER_PRINT(line);
-  snprintf(line, line_byte_size, ",{\"ph\":\"%c\",\"ts\":%lu,\"pid\":%d,\"tid\":%d,\"name\":\"%s\"}\n", 'E', _TINYPROFILER_DIFF((unsigned long)_prof_time()), 0, 0, __func__); _TINYPROFILER_PRINT(line);
-  snprintf(line, line_byte_size, "]}\n"); _TINYPROFILER_PRINT(line);
+  snprintf(line, line_bytes, ",{\"ph\":\"%c\",\"ts\":%lu,\"pid\":%d,\"tid\":%d,\"name\":\"%s\"}\n", 'B', TINYPROFILER_TIME_TO_TS((unsigned long)self_t_begin), 0, 0, __func__); TINYPROFILER_OUTPUT_STRING(line);
+  snprintf(line, line_bytes, ",{\"ph\":\"%c\",\"ts\":%lu,\"pid\":%d,\"tid\":%d,\"name\":\"%s\"}\n", 'E', TINYPROFILER_TIME_TO_TS((unsigned long)_prof_time()), 0, 0, __func__); TINYPROFILER_OUTPUT_STRING(line);
+  snprintf(line, line_bytes, "]}\n"); TINYPROFILER_OUTPUT_STRING(line);
+  for (int t = 0; t < TINYPROFILER_MAX_NUM_OF_THREADS; t += 1) { free(_prof_data[t].s); }
   free(line);
 }
 
-static inline void profB(char * name) { _prof(0, 'B', (unsigned long)_prof_time(), 0, 0, sizeof(name), name); }
-static inline void profE(char * name) { _prof(0, 'E', (unsigned long)_prof_time(), 0, 0, sizeof(name), name); }
-static inline void profBmt(int tid, char * name) { _prof(tid, 'B', (unsigned long)_prof_time(), 0, tid, sizeof(name), name); }
-static inline void profEmt(int tid, char * name) { _prof(tid, 'E', (unsigned long)_prof_time(), 0, tid, sizeof(name), name); }
+#ifndef profB
+#define profB(name) _prof(0, 'B', (unsigned long)_prof_time(), 0, 0, sizeof(name), name)
+#endif
 
-#else // USE_TINYPROFILER
+#ifndef profE
+#define profE(name) _prof(0, 'E', (unsigned long)_prof_time(), 0, 0, sizeof(name), name)
+#endif
 
-static inline void profAlloc(size_t sample_count_per_thread) {}
-static inline void profPrintAndFree() {}
-static inline void profB(char * name) {}
-static inline void profE(char * name) {}
-static inline void profBmt(int tid, char * name) {}
-static inline void profEmt(int tid, char * name) {}
+#ifndef profBmt
+#define profBmt(tid, name) _prof(tid, 'B', (unsigned long)_prof_time(), 0, tid, sizeof(name), name)
+#endif
+
+#ifndef profEmt
+#define profEmt(tid, name) _prof(tid, 'E', (unsigned long)_prof_time(), 0, tid, sizeof(name), name)
+#endif
 
 #endif // USE_TINYPROFILER
